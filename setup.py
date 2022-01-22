43f9315a39399c55d4f05e85146d779b3cc1a364
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-01-22 16:25:49 -0300

    Upgrade version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name="pydrawer",
     packages=["pydrawer"],
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     description="The Python package for visualizing curves and linear transformations in a super simple way.",
     author="Dylan Tintenfich",
