b647e0bcc76b7498f4026ba33a77f8596e88b0f6
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-02-21 19:32:49 -0300

    Improve examples

--- examples/commutative_property.py
+++ examples/commutative_property.py
@@ -1,16 +1,46 @@
 import numpy as np
-from curvipy import GraphingCalculator
+from curvipy import GraphingCalculator, graphing_calculator
 from curvipy import lintrans
-from time import sleep
+
+
+# In this example we are going to use curvipy to prove that not all matrix
+# multiplication is commutative.
 
 """
-This example uses the numpy library. If you don't have it already you can simply
-install it with pip:
-$ pip install numpy
+To visualize how matrix multiplication works, we are going to use the curve
+f(t) = <t, t> (which is the function y=x+10 parametrized).
+
+As explained on lintrans.md (https://github.com/dylannalex/curvipy/blob/master/docs/lintrans.md)
+we can think of f(t) graph as the curve formed by drawing all the f(t) image
+vectors, and then joining their heads.
+"""
+
+graphing_calculator = GraphingCalculator(
+    background_color="black",
+    curve_color="#FF5A5F",
+    curve_width=10,
+    vector_color="#00A61C",
+    vector_width=3,
+    vector_head_size=10,
+    show_axis=True,
+    axis_color="grey",
+    axis_width=2,
+)
 
 
-In this example we are going to see how not all matrices multiplication are
-commutative. For doing it we are going to define two matrices:
+def f(t):
+    """
+    This curve represents the y=x+10 function
+    """
+    return t - 10, t
+
+
+graphing_calculator.draw_animated_curve(f, (-20, 30))
+input("Press Enter to continue...")
+graphing_calculator.clear()
+
+"""
+For doing this let's define two matrices:
 
                     A = [ 0  1 ]        B = [ 1  1 ] 
                         [-1  0 ]            [ 0  1 ] 
@@ -18,23 +48,13 @@ commutative. For doing it we are going to define two matrices:
 - A rotates vectors 90° anticlockwise
 - B is a shear matrix (https://en.wikipedia.org/wiki/Shear_matrix)
 
-To visualize how matrix multiplication works, we are going to use the curve
-f(t) = <t, t> (the y = x function). 
-We can think of f(t) as the curve formed by joining all the <t, t> vectors' head.
-Applying a matrix to f(t) means that we are applying a linear transformation to
-each vector and then joining their heads again.
+If AB = BA, transforming f(t) by AB would generate the same function
+as transforming f(t) by BA.
 
-Lets see if applying the AB matrix to f(t) gives the same transformed curve as
-if we apply the BA matrix!
+NOTE: keep in mind that applying linear transformations to parametrized
+functions means applying the linear transformation to each vector of the
+parametrized function. Again, check out lintrans.md for more info.
 """
-
-
-def f(t):
-    """
-    This curve represents the y=x function
-    """
-    return t, t
-
 
 A = np.array(((0, 1), (-1, 0)))
 B = np.array(((1, 1), (0, 1)))
@@ -43,21 +63,17 @@ AB = np.matmul(A, B)
 BA = np.matmul(B, A)
 
 
-calc = GraphingCalculator(
-    background_color="black", curve_color="#2CFF2C", curve_width=9
-)
-
-print("Drawing original curve")
-calc.draw(f, (-20, 20))
-sleep(2)
-calc.clear()
-
 print("Drawing curve transformed by matrix AB")
-calc.draw(lintrans.transform_curve(f, AB), (-20, 20))
-sleep(2)
-calc.clear()
+graphing_calculator.draw_animated_curve(lintrans.transform_curve(f, AB), (-20, 30))
+input("Press Enter to continue...")
+graphing_calculator.clear()
+
 
 print("Drawing curve transformed by matrix BA")
-BA = np.matmul(B, A)
-calc.draw(lintrans.transform_curve(f, BA), (-20, 20))
-sleep(2)
+graphing_calculator.draw_animated_curve(lintrans.transform_curve(f, BA), (-20, 30))
+input("Press Enter to continue...")
+
+"""
+As you could see, f(t) transformed by AB is different from f(t) transformed
+by BA. This means we find a counterexample for AB = BA.
+"""
