feef62f26ee977062eea70fadecd579f72a02d57
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2022-10-10 20:09:32 -0300

    Update plotter module
    - Add curve, interval and vector modules functionalities.
    - Add documentation.
    - Fix minor bugs.

--- curvipy/plotter.py
+++ curvipy/plotter.py
@@ -1,29 +1,55 @@
-from typing import Any, Callable
-from math import sqrt, sin, acos, cos, pi
-import turtle
+import turtle as _turtle
+from math import sin as _sin
+from math import cos as _cos
+from math import pi as _pi
+
+from .vector import Vector as _Vector
+from .curve import Curve as _Curve
+from .interval import Interval as _Interval
 
 
-def _is_int(result: Any) -> bool:
-    try:
-        int(result)
-        return True
-    except Exception:
-        return False
+class Plotter(_turtle.Turtle):
+    """Graph plotter for drawing vectors and curves. Inherits from `turtle.Turtle` \
+    class.
+        
+    Parameters
+    ----------
+    window_title : str
+        Title to display on window. Defaults to "curvipy".
+    plotting_speed : int
+        Curve plotting speed. Defaults to 11.
+    background_color : str
+        Background color. Can either be a name (e.g. "Blue") or a hex color code. \
+        Defaults to "#F1FAEE".
+    curve_color : str
+        Curve color. Can either be a name (e.g. "Red") or a hex color code. Defaults \
+        to "#457B9D".
+    curve_width : int
+        Curve width. Defaults to 4.
+    vector_color : str
+        Vector color. Can either be a name (e.g. "Yellow") or a hex color code. Defaults \
+        to "#E63946".
+    vector_width : int
+        Vector width. Defaults to 3.
+    vector_head_size : int
+        Size of vectors' head. Defaults to 10.
+    show_axis : bool
+        If true, x-axis and y-axis are shown. Defaults to True.
+    axis_color : str
+        Axis color. Can either be a name (e.g. "Green") or a hex color code. Defaults \
+        to "#A8DADC".
+    axis_width : int
+        Axis width. Defaults to 2.
+    x_axis_scale : int
+        X-axis scale. Defaults to 10.
+    y_axis_scale : int
+        Y-axis scale. Defaults to 10.
+    """
 
-
-def _is_float(result: Any) -> bool:
-    try:
-        float(result)
-        return True
-    except Exception:
-        return False
-
-
-class Curvipy(turtle.Turtle):
     def __init__(
         self,
-        window_title: str = "curvipy",
-        drawing_speed: int = 11,
+        window_title: str = "Curvipy",
+        plotting_speed: int = 11,
         background_color: str = "#F1FAEE",
         curve_color: str = "#457B9D",
         curve_width: int = 4,
@@ -33,26 +59,18 @@ class Curvipy(turtle.Turtle):
         show_axis: bool = True,
         axis_color: str = "#A8DADC",
         axis_width: int = 2,
+        x_axis_scale: int = 10,
+        y_axis_scale: int = 10,
     ) -> None:
-        """
-        :param window_title: title to display on window
-        :param drawing_speed: integer value
-        :param background_color: color name or hex code
-        :param curve_color: color name or hex code
-        :param curve_width: integer value that specifies curve width
-        :param show_axis: draws axis if true
-        :param axis_color: color name or hex code
-        :param axis_width: axis value that specifies curve width
-        """
 
-        turtle.Turtle.__init__(self)
+        _turtle.Turtle.__init__(self)
         self.shapesize(0.1, 0.1, 0.1)
         self.shape("square")
-        turtle.title(window_title)
-        turtle.bgcolor(background_color)
+        _turtle.title(window_title)
+        _turtle.bgcolor(background_color)
 
         # Screen attributes
-        self.drawing_speed = drawing_speed
+        self.plotting_speed = plotting_speed
         self.background_color = background_color
 
         # Curves attributes
@@ -68,155 +86,11 @@ class Curvipy(turtle.Turtle):
         self.axis_color = axis_color
         self.show_axis = show_axis
         self.axis_width = axis_width
+        self.x_axis_scale = x_axis_scale
+        self.y_axis_scale = y_axis_scale
 
         if self.show_axis:
             self._draw_axis()
-
-    def draw_vector(
-        self,
-        head: tuple[float, float],
-        tail: tuple[float, float] = (0, 0),
-        x_axis_scale: int = 10,
-        y_axis_scale: int = 10,
-    ) -> None:
-
-        vector_coordinates = (head[0] - tail[0], head[1] - tail[1])
-
-        # Check if vector is the cero vector (v = [0, 0])
-        vector_norm = sqrt(vector_coordinates[0] ** 2 + vector_coordinates[1] ** 2)
-        if vector_norm == 0:
-            return
-
-        # Graphing calculator setup
-        self.color(self.vector_color)
-        self.width(self.vector_width)
-
-        # Draw vector
-        scaled_tail = (tail[0] * x_axis_scale, tail[1] * y_axis_scale)
-        self._goto_without_drawing(scaled_tail)
-        scaled_head = (head[0] * x_axis_scale, head[1] * y_axis_scale)
-        self._goto_drawing(scaled_head)
-
-        # Draw vector head
-        vector_angle = acos(vector_coordinates[0] / vector_norm)
-
-        if vector_coordinates[1] < 0:
-            vector_angle *= -1
-
-        left_head_vector = (
-            self.vector_head_size * cos(vector_angle + pi * 5 / 4),
-            self.vector_head_size * sin(vector_angle + pi * 5 / 4),
-        )
-        left_head_endpoint = (
-            scaled_head[0] + left_head_vector[0],
-            scaled_head[1] + left_head_vector[1],
-        )
-
-        self._goto_drawing(left_head_endpoint)
-
-        right_head_vector = (
-            self.vector_head_size * cos(vector_angle - pi * 5 / 4),
-            self.vector_head_size * sin(vector_angle - pi * 5 / 4),
-        )
-        right_head_endpoint = (
-            scaled_head[0] + right_head_vector[0],
-            scaled_head[1] + right_head_vector[1],
-        )
-
-        self._goto_without_drawing(scaled_head)
-        self._goto_drawing(right_head_endpoint)
-
-    def draw_curve(
-        self,
-        curve: Callable,
-        domain_interval: tuple[int, int],
-        x_axis_scale: int = 10,
-        y_axis_scale: int = 10,
-    ) -> None:
-        """
-        Determines if a given curve is a function or a parametrized curve and
-        draws it.
-
-        :param curve: parametrized curve or function to draw
-        :param domain_interval: curve domain interval
-        :param x_axis_scale: x axis scaling factor
-        :param y_axis_scale: y axis scaling factor
-        """
-        curve_evaluation = curve(domain_interval[0])
-        if _is_int(curve_evaluation) or _is_float(curve_evaluation):
-            self._draw_function(curve, domain_interval, x_axis_scale, y_axis_scale)
-        elif len(curve_evaluation) == 2:
-            self._draw_parametrized_function(
-                curve, domain_interval, x_axis_scale, y_axis_scale
-            )
-        else:
-            raise ValueError("'curve' should be a function or parametrized curve")
-
-    def draw_animated_curve(
-        self,
-        parametrized_function: Callable,
-        domain_interval: tuple[int, int],
-        vector_frequency: int = 2,
-        x_axis_scale: int = 10,
-        y_axis_scale: int = 10,
-    ) -> None:
-        """
-        Given a parametrized function f(t) = [x(t), y(t)], draws a the
-        set of vectors {[x(t), y(t)] | t e domain_interval} and then
-        draws f(t) graph.
-
-        :param parametrized_function: curve to draw
-        :param domain_interval: curve domain interval
-        :param vector_frequency: the frequency which vectors will be
-        drawn. The lower frequency the more vectors
-        :param x_axis_scale: x axis scaling factor
-        :param y_axis_scale: y axis scaling factor
-        """
-        for t in range(domain_interval[0], domain_interval[1] + 1, vector_frequency):
-            f_vector = parametrized_function(t)
-            self.draw_vector(f_vector, (0, 0), x_axis_scale, y_axis_scale)
-        self._draw_parametrized_function(
-            parametrized_function, domain_interval, x_axis_scale, y_axis_scale
-        )
-
-    def _draw_function(
-        self,
-        f: Callable,
-        domain_interval: tuple[int, int],
-        x_axis_scale: int,
-        y_axis_scale: int,
-    ) -> None:
-        self.color(self.curve_color)
-        self.width(self.curve_width)
-        for x in range(domain_interval[0], domain_interval[1] + 1, 1):
-            f_point = (
-                x_axis_scale * x,
-                y_axis_scale * f(x),
-            )
-            if x == domain_interval[0]:
-                self._goto_without_drawing(f_point)
-            else:
-                self._goto_drawing(f_point)
-
-    def _draw_parametrized_function(
-        self,
-        parametrized_function: Callable,
-        domain_interval: tuple[int, int],
-        x_axis_scale: int,
-        y_axis_scale: int,
-    ) -> None:
-        self.color(self.curve_color)
-        self.width(self.curve_width)
-        for t in range(domain_interval[0], domain_interval[1] + 1, 1):
-            f_vector = parametrized_function(t)
-            scaled_f_vector = (
-                x_axis_scale * f_vector[0],
-                y_axis_scale * f_vector[1],
-            )
-            if t == domain_interval[0]:
-                self._goto_without_drawing(scaled_f_vector)
-            else:
-                self._goto_drawing(scaled_f_vector)
 
     def _goto_without_drawing(self, position: tuple[int, int]) -> None:
         self.speed("fastest")
@@ -225,7 +99,7 @@ class Curvipy(turtle.Turtle):
         self.down()
 
     def _goto_drawing(self, position: tuple[int, int]) -> None:
-        self.speed(self.drawing_speed)
+        self.speed(self.plotting_speed)
         self.down()
         self.goto(position)
 
@@ -233,7 +107,7 @@ class Curvipy(turtle.Turtle):
         self.width(self.axis_width)
         self.color(self.axis_color)
 
-        w, h = turtle.screensize()
+        w, h = _turtle.screensize()
 
         # y axis
         self._goto_without_drawing((0, -h * 2))
@@ -243,11 +117,118 @@ class Curvipy(turtle.Turtle):
         self._goto_without_drawing((-w * 2, 0))
         self.goto((w * 2, 0))
 
+    def plot_vector(self, vector: _Vector) -> None:
+        """Plots the given two-dimensional vector.
+
+        Parameters
+        ----------
+        vector : Vector
+            Vector to be plotted.
+        """
+        # Check if vector is the cero vector (v = [0, 0])
+        if not vector.norm:
+            return
+
+        # Turtle settings
+        self.color(self.vector_color)
+        self.width(self.vector_width)
+
+        # Draw vector
+        scaled_tail = (
+            vector.tail[0] * self.x_axis_scale,
+            vector.tail[1] * self.y_axis_scale,
+        )
+        self._goto_without_drawing(scaled_tail)
+        scaled_head = (
+            vector.head[0] * self.x_axis_scale,
+            vector.head[1] * self.y_axis_scale,
+        )
+        self._goto_drawing(scaled_head)
+
+        # Draw vector head
+        left_head_vector = (
+            self.vector_head_size * _cos(vector.angle + _pi * 5 / 4),
+            self.vector_head_size * _sin(vector.angle + _pi * 5 / 4),
+        )
+        left_head_endpoint = (
+            scaled_head[0] + left_head_vector[0],
+            scaled_head[1] + left_head_vector[1],
+        )
+
+        self._goto_drawing(left_head_endpoint)
+
+        right_head_vector = (
+            self.vector_head_size * _cos(vector.angle - _pi * 5 / 4),
+            self.vector_head_size * _sin(vector.angle - _pi * 5 / 4),
+        )
+        right_head_endpoint = (
+            scaled_head[0] + right_head_vector[0],
+            scaled_head[1] + right_head_vector[1],
+        )
+
+        self._goto_without_drawing(scaled_head)
+        self._goto_drawing(right_head_endpoint)
+
+    def plot_curve(self, curve: _Curve, interval: _Interval) -> None:
+        """Plots the given two-dimensional curve in the specified interval.
+
+        Parameters
+        ----------
+        curve : Curve
+            Curve to be plotted.
+        interval : Interval
+            Interval from which the curve will be plotted.
+        """
+        # Turtle settings
+        self.color(self.curve_color)
+        self.width(self.curve_width)
+
+        # Curve points
+        points = curve.points(interval)
+        start_point = points[0]
+        scaled_start_point = (
+            self.x_axis_scale * start_point[0],
+            self.y_axis_scale * start_point[1],
+        )
+
+        # Draw curve
+        self._goto_without_drawing(scaled_start_point)
+        for point in points[1:]:
+            scaled_point = (
+                self.x_axis_scale * point[0],
+                self.y_axis_scale * point[1],
+            )
+            self._goto_drawing(scaled_point)
+
+    def plot_animated_curve(
+        self, curve: _Curve, interval: _Interval, samples_per_vector: int
+    ) -> None:
+        """Plots the given curve by drawing a set of vectors pointing at the curve \
+        points and then joining the vector heads.
+
+        Parameters
+        ----------
+        curve : Curve
+            Curve to be plotted.
+        interval : Interval
+            Interval from which the curve will be plotted.
+        samples_per_vector : int
+            Number of samples per each vector plotted. The less `samples_per_vector` \
+            is, the more vectors are drawn. 
+        """
+        # Plot vectors:
+        for i, vector in enumerate(curve.points(interval)):
+            if i % samples_per_vector != 0:
+                continue
+
+            self.plot_vector(_Vector(vector))
+
+        # Plot curve:
+        self.plot_curve(curve, interval)
+
     def clean(self) -> None:
-        """
-        Cleans the graphic calculator screen.
-        """
+        """Removes curves and vectors plotted."""
         self.clear()
-        turtle.bgcolor(self.background_color)
+        _turtle.bgcolor(self.background_color)
         if self.show_axis:
             self._draw_axis()
