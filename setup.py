12f6b6888b5a80ff868a778600815d72871e1530
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2022-10-18 11:33:14 -0300

    Upgrade version to 1.0.0

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
     name="curvipy",
     packages=["curvipy"],
-    version="0.1.1",
+    version="1.0.0",
     license="MIT",
-    description="The Python package for visualizing mathematical functions, curves and linear transformations with animations.",
+    description="The Python library for making math animations in a few lines of code.",
     author="Dylan Tintenfich",
     author_email="tintenfichdylan05@gmail.com",
     url="https://github.com/dylannalex/curvipy",
-    keywords=["calculus", "linear-algebra", "matrix", "mathematics"],
+    keywords=["curves", "linear-algebra", "mathematics", "animations"],
     install_requires=["PyYAML==6.0", "turtle==0.0.1"],
     classifiers=[
         "Programming Language :: Python :: 3.9",
