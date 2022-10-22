ffae3ee09de81b55ec6fc9f66d4393cd954c23ce
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2022-10-22 15:14:10 -0300

    Fix release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "curvipy"
 copyright = "2022, Dylan Tintenfich"
 author = "Dylan Tintenfich"
-release = "2.0.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
